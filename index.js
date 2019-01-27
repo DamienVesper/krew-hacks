8a4fe5137ecea65a24a1f9ad87f50453717b4e9b
Damien Vesper <34838468+DamienVesper@users.noreply.github.com>
2019-01-27 17:02:39 -0600

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@
 // @author       DamienVesper
 // @match        *://krew.io/*
 // @match        *://*.krew.io/*
+// @updateURL    https://raw.githubusercontent.com/DamienVesper/krewio-hacks/master/index.js
 // @grant        none
 // ==/UserScript==
 (function() {
