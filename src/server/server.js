cdede336e1d3458023f031949a444cfb887e88b4
Damien Vesper <ldamienvesper@gmail.com>
2021-03-20 21:43:15 -0500

    add server

--- src/server/server.js
+++ src/server/server.js
@@ -1,2 +1,22 @@
+require(`dotenv`).config();
+const path = require(`path`);
+
 const express = require(`express`);
 const app = express();
+
+const http = require(`http`);
+const server = http.createServer(app);
+
+// Set view engine.
+app.set(`views`, path.resolve(__dirname, `views`));
+app.set(`view engine`, `ejs`);
+
+// Serve the static directory.
+app.use(express.static(config.staticDir));
+
+app.get(`/`, (req, res) => res.render(`index.ejs`));
+
+module.exports = {
+    app,
+    server
+};
