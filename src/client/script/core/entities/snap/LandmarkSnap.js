527509ef62a7fc93f58cae13167be2b4a2215780
Damien Vesper <ldamienvesper@gmail.com>
2021-06-24 16:01:07 -0500

    Revert "add client src"
    This reverts commit dc4ffcc2a590ec61ebd0319a00d215e34ae0fac2.

--- src/client/script/core/entities/snap/LandmarkSnap.js
+++
@@ -1,9 +0,0 @@
-let LandmarkSnap = {
-    /**
-     * Method to parse a landmark type snap
-     *
-     * @param {object} snap Snap to be parsed
-     * @param {object} _this Landmark object
-     */
-    parseTypeSnap: (snap, _this) => {}
-};
